9114dcf4d6295ce603e685ec24dc93bb83d5620d
Samuel Boulery <samuelboulery@gmaill.com>
2020-03-03 16:02:05 +0100

    login pages done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
     # devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar])
   end
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a? Photographer
+      stored_location_for(resource) || professional_root_path
+    else
+      stored_location_for(resource) || director_root_path
+    end
+  end
 end
