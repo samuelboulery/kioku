6d3f5a2cff0e0545347de8fb9284cb5dae75c6b3
marie <marie.blanchet.pro@gmail.com>
2020-03-04 18:50:57 +0100

    debut dragndrop

---
+++ app/javascript/javascripts/dragndrop.js
@@ -0,0 +1,30 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("drag");
+
+  if (form) {
+    var fileInput = document.querySelector('input[type=file]');
+    var filenameContainer = document.querySelector('#filename');
+    var dropzone = document.querySelector('.dragndrop-form');
+
+
+    fileInput.addEventListener('change', function() {
+      var files = fileInput.files;
+      console.log(files);
+      for (var i = 0; i < files.length; i++) {
+        // on récupère le i-ème fichier
+        file = files.item(i);
+        // // ou encore
+        // file = files[i];
+        filenameContainer.insertAdjacentHTML("beforeend", file.name);
+      }
+    });
+
+    fileInput.addEventListener('dragenter', function() {
+      dropzone.classList.add('dragover');
+    });
+
+    fileInput.addEventListener('dragleave', function() {
+      dropzone.classList.remove('dragover');
+    });
+  };
+});
