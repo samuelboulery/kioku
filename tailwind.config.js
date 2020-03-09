c4e72f07771ee2cdd910732927525e9bd262b894
Clem <clem2486@gmail.com>
2020-03-09 16:15:01 +0100

    show school

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
+const colors = {
+  purple: "#7D07F2",
+  red: "#FB4953",
+  yellow: "#FEC957",
+  green: "#65F1E4"
+}
+
 module.exports = {
   theme: {
     extend: {
       borderRadius: {
-      'lg': '2rem'
-    },
-      colors: {
-        purple: "#7D07F2",
-        red: "#FB4953",
-        yellow: "#FEC957",
-        green: "#65F1E4",
+        'lg': '2rem'
       },
-    },
+      colors: colors,
+      fill: colors
+    }
   },
   variants: {},
   plugins: [],
