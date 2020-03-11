b9667ac798c7503e8f7d78f7e3a1ec8bdbc3d0d3
marie <marie.blanchet.pro@gmail.com>
2020-03-11 15:52:43 +0100

    add metas and favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
 
    before_action :configure_permitted_parameters, if: :devise_controller?
 
-def configure_permitted_parameters
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
     # devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar])
   end
